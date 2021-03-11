80241ef8050e241de696b3a3491dd960f2b329cb
Ivan <last1samyrai@gmail.com>
2021-03-12 01:16:26 +0200

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = 0;
+    let num = Math.abs(n);
+    while (num) {
+        result = result * 10 + num % 10;
+        num = Math.floor(num/10);
+    }
+    return result;
 }
